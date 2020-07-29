2439de5b539e65c3dddbb043f37b0fb539b2ac90
Francis <francis.munabeno@yahoo.fr>
2020-07-29 09:57:38 +0200

    update set view ejs

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var express = require("express")
 var app = express()
 var fetch = require('node-fetch')
 var bodyParser = require("body-parser");
-var { config, obj } = require('./js/app.js')
+var { config, obj } = require('./public/js/app.js')
 var { GET_URL_STUDENT,
       POST_URL_STUDENT,
       GET_URL_GROUP, 
       POST_URL_GROUP
-    } = require('./js/const.js')
+    } = require('./public/js/const.js')
 
  
 // PORT SERVE
@@ -19,13 +19,14 @@ app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //SET
+app.set('views', './public');
 app.set('view engine', 'ejs')
 
 app.get('/student', async(req,res) => {
   try{
     const getStudent = await fetch(GET_URL_STUDENT)
     const parseStudent = await getStudent.json()
-    res.render('pages/student', { data : parseStudent }) 
+    res.render('views/pages/student', { data : parseStudent }) 
   }catch(err){
     console.log(err)
   }
@@ -35,7 +36,7 @@ app.get('/group', async(req,res) => {
   try{
     const getGroup = await fetch(GET_URL_GROUP)
     const parseGroup = await getGroup.json()
-    res.render('pages/group', { data : parseGroup }) 
+    res.render('views/pages/group', { data : parseGroup }) 
   }catch(err){
     console.log(err)
   }
